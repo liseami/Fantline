ee27d75c2ce399642f6e2853e6faf1a86fc61d30
liseami <51239614+liseami@users.noreply.github.com>
2021-12-29 10:09:06 +0800

    32

---
+++ Fantline/Fantline/Views/ProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileView.swift
+//  Fantline
+//
+//  Created by 赵翔宇 on 2021/12/29.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
